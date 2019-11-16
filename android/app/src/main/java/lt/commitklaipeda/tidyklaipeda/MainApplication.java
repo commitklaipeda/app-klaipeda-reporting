229353d836c072bc3450a8c804a12694b571f99f
Deimantas Špučys <as.deimis@gmail.com>
2019-11-16 20:18:18 +0200

    added missing rn module links

--- android/app/src/main/java/lt/commitklaipeda/tidyklaipeda/MainApplication.java
+++ android/app/src/main/java/lt/commitklaipeda/tidyklaipeda/MainApplication.java
@@ -3,6 +3,7 @@ package lt.commitklaipeda.tidyklaipeda;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReanimatedPackage(),
             new VectorIconsPackage(),
             new MapsPackage(),
             new RNGestureHandlerPackage()
